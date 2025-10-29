17ebfb53f805c7a03c039735eab847c478e3c8ae
Mikos22 <mikailuysal2006@gmail.com>
2025-10-29 10:52:58 +0100

    routes i app.js

--- projekt/app.js
+++ projekt/app.js
@@ -4,7 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var customerRouter = require('./routes/customer');
+var firmRouter = require('./routes/customer');
+var dbRouter = require('./routes/customer');
 
 var app = express();
 
@@ -15,6 +17,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/customer', customerRouter);
+app.use('/firm', firmRouter);
+app.use('/db', dbRouter);
 
 module.exports = app;
