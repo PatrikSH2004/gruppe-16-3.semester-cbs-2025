f7c1469064d18235028b668c94ad7d4ce01c8d3a
PatrikSH2004 <paha24ad@student.cbs.dk>
2025-10-29 16:25:07 +0100

    Update firm.js

--- projekt/routes/firm.js
+++ projekt/routes/firm.js
@@ -1,10 +1,11 @@
 /* Routes for customer-related operations */
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/home', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/pages/company/dashboard.html'));
 });
 
 module.exports = router;
