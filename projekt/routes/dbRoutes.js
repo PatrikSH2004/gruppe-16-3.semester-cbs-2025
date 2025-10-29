2cb8d7fc8474d8bda5e12f12d660071c302cabee
Mikos22 <mikailuysal2006@gmail.com>
2025-10-29 11:28:11 +0100

    hovedside done

--- projekt/routes/dbRoutes.js
+++ projekt/routes/dbRoutes.js
@@ -1,1 +1,10 @@
 /* Routes for DB */ 
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
